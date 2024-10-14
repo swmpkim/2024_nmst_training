a78c0349b27d3106048f238e62952029b735db47
Kim Cressman <kim@catbirdstats.com>
2024-10-14 09:55:12 -0500

    initial commit of R files

---
+++ R/02_run-Data-Formatting-App.R
@@ -0,0 +1,20 @@
+# Shiny app to export CDMO-formatted data
+# from NMST-formatted file
+
+#### INSTRUCTIONS ############################################################
+
+# 1:
+# Re-start your R session to make sure there's no interference:
+# From the menu bar, select 'Session', then 'Restart R'
+# Windows keyboard shortcut is Ctrl + Shift + F10
+
+
+# 2:
+# Run the line of code below. It will cause the app to run in its
+# own pop-up browser window.
+
+##############################################################################
+
+
+# Call app ----
+source(here::here("R", "02_dataFormattingAppDirectory", "001_runApp.R"))
